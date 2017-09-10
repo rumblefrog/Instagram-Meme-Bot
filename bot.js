edd4916678e1a356cd9ef0eb81b9f22f2dbc55dc
RumbleFrog <admin@biturl.io>
2017-09-09 23:57:40 -0400

    Debug Messages | Removed Old Code

--- bot.js
+++ bot.js
@@ -30,26 +30,6 @@ log.add(log.transports.File, {
     timestamp: true
 });
 
-// Client.Session.create(device, storage, config.credentials.username, config.credentials.password)
-//     .then((session) => {
-//         getReddit((err, result) => {
-//             if (!err) {
-//                 let stream = temp.createWriteStream({prefix:'image',suffix:'.jpg'});
-//                 request(result.data.url).pipe(stream).on('close', () => {
-//                     stream.end();
-//                     Client.Upload.photo(session, stream.path)
-//                         .then((upload) => {
-//                             console.log(upload.params.uploadId);
-//                             return Client.Media.configurePhoto(session, upload.params.uploadId, result.data.title);
-//                         })
-//                         .then((medium) => {
-//                             console.log(medium.params)
-//                         })
-//                 });
-//             }
-//         });
-//     })
-
 var IG;
 
 Client.Session.create(device, storage, config.credentials.username, config.credentials.password)
@@ -59,6 +39,7 @@ Client.Session.create(device, storage, config.credentials.username, config.crede
 
         IG = session;
 
+        log.info('Scheduled jobs for uploads');
         schedule.scheduleJob('0 13 * * *', uploadIF);
         schedule.scheduleJob('0 16 * * *', uploadIF);
         schedule.scheduleJob('0 19 * * *', uploadIF);
@@ -75,8 +56,12 @@ function uploadIF() {
 
             if (!IF_Cache.existsSync(meta.contentId)) {
 
+                log.debug('Post candidate found');
+
                 IF_Cache.push(meta.contentId, (err) => {
                     if (err) return;
+
+                    log.debug('Candidate type: ' + meta.contentType);
 
                     let dl_src = (meta.contentType == 'video_clip') ? memes[i].video : memes[i].image;
 
@@ -90,6 +75,7 @@ function uploadIF() {
 
                         if (final_type == 'image') {
                             let cropped = temp.createWriteStream({prefix:'ifunny',suffix:'.jpg'});
+                            log.debug('Cropping IF watermark for image upload');
                             const image = sharp(fs.readFileSync(stream.path));
                             image
                                 .metadata()
@@ -101,6 +87,7 @@ function uploadIF() {
                                 })
                                 .then((data) => {
                                     cropped.end();
+                                    log.debug('Uploading');
                                     Client.Upload.photo(IG, cropped.path)
                                         .then((upload) => {
                                             Client.Media.configurePhoto(IG, upload.params.uploadId, Modules.IF.generateCaption(memes[i].tags, config.credentials.username));
@@ -109,6 +96,7 @@ function uploadIF() {
                         } else {
                             if (dl_ext == '.gif') {
                                 let convert = temp.createWriteStream({prefix:'ifunny',suffix:'.mp4'});
+                                log.debug('Converting gif -> video');
                                 ffmpeg(stream.path)
                                     .output(convert)
                                     .on('end', () => {
@@ -116,12 +104,13 @@ function uploadIF() {
                                     })
                                     .on('codecData', (data) => {
                                         let thumbnail = temp.createWriteStream({prefix:'ifunny',suffix:'.jpg'});
+                                        log.debug('Converting thumbnail for gif -> video upload');
                                         ffmpeg(stream.path)
                                             .frames(1)
                                             .output(thumbnail)
                                             .on('end', () => {
                                                 thumbnail.end();
-
+                                                log.debug('Uploading');
                                                 Client.Upload.video(IG, convert.path, thumbnail.path)
                                                     .then((upload) => {
                                                         Client.Media.configureVideo(IG, upload.uploadId, Modules.IF.generateCaption(memes[i].tags, config.credentials.username), data.duration);
@@ -131,9 +120,11 @@ function uploadIF() {
                             } else {
                                 let thumbnail = temp.createWriteStream({prefix:'ifunny',suffix:'.jpg'});
 
+                                log.debug('Downloading thumbnail for video upload');
                                 request(memes[i].image).pipe(thumbnail).on('close', () => {
                                     thumbnail.end();
                                     getDuration(stream.path).then((duration) => {
+                                        log.debug('Uploading');
                                         Client.Upload.video(IG, stream.path, thumbnail.path)
                                             .then((upload) => {
                                                 Client.Media.configureVideo(IG, upload.uploadId, Modules.IF.generateCaption(memes[i].tags, config.credentials.username), duration);
@@ -144,10 +135,9 @@ function uploadIF() {
                         }
                     });
                 });
+                log.info('Schedule ran');
                 break;
             }
         }
     });
 }
-
-//uploadIF();
